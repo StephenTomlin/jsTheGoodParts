964defd5e40b6578d4a41a410f4b4fd296825be8
StephenTomlin <Stephen.Tomlinson@live.com>
2017-02-10 22:24:24 -0500

    wrote better code making cat a subclass of mammal

--- pseudoclassical/program.js
+++ pseudoclassical/program.js
@@ -69,3 +69,29 @@ var purr = myCat.purr(5); // 'r-r-r-r-r'
 var name = myCat.get_name();
 //             'meow Henrietta meow'
 
+Function.method('inherits', function (Parent) {
+  this.prototype = new Parent();
+  return this;
+});
+
+// Our inherits and method methods return this, allowing us to program
+// in a cascade style. We can now make our Cat with one statement
+
+var Cat = function (name) {
+  this.name = name;
+  this.saying = 'meow';
+}.
+  inherits(Mammal).
+  method('purr', function(n) {
+    var i, s = '';
+    for (i = 0; i < n; i += 1) {
+      if (s) {
+        s += '-';
+      }
+      s += 'r';
+    }
+    return s;
+  }).
+  method('get_name', function () {
+    return this.says() + ' ' + this.name + ' ' + this.says();
+  });
